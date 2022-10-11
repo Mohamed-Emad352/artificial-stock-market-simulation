dc4d389d82ce5c1ed650505be37982554df746c7
omnia ehab <omnia.ehab232@gmail.com>
2022-10-10 19:32:56 -0700

    implement-fundamentalist-and-chartists

--- src/Agents/Chartists/Chartist.java
+++ src/Agents/Chartists/Chartist.java
@@ -1,4 +1,49 @@
 package Agents.Chartists;
+import Agents.Trader;
+import Configurations.ChartistAgentConfiguration;
+import Configurations.TraderConfiguration;
+import Enums.Decision;
 
-public class Chartist {
+import static java.lang.Math.abs;
+public class Chartist extends Trader {
+    ChartistAgentConfiguration configuration;
+    Integer movingAverageWindowSize ;
+
+    public Chartist(TraderConfiguration config) {
+        super(config);
+        movingAverageWindowSize = configuration.movingAverageWindow;
+
+    }
+
+    @java.lang.Override
+    public Decision decideBuyOrSell() {
+        Double value;
+        value = configuration.market.getCurrentPrice() - getMovingAverage();
+        // apply sign (sgn) function to the value to determine the direction H
+        if(value > 0) // direction = 1;
+        {return Decision.Buy;}
+
+        else if(value < 0) //direction = -1
+        { return Decision.Sell;}
+
+        else {return null;} ///
+    }
+
+    @java.lang.Override
+    public Integer getDesiredOrderVolume() {
+        Double m = getMovingAverage();
+        return (int)abs(configuration.ReactionCoefficient * (configuration.market.getCurrentPrice() - m));
+    }
+
+    Double getMovingAverage(){
+        Double MA ;
+        Double summationOfPrices = 0.0;
+        int Day = configuration.market.getCurrentDay();
+        for (int i=1; i<= movingAverageWindowSize; i++){
+            summationOfPrices += configuration.market.getPriceFromList(Day-i);
+        }
+        MA = summationOfPrices / movingAverageWindowSize;
+        return MA;
+
+    }
 }
--- src/Agents/Fundamentalists/Fundamentalist.java
+++ src/Agents/Fundamentalists/Fundamentalist.java
@@ -1,6 +1,47 @@
 package Agents.Fundamentalists;
 
 import Agents.Trader;
+import Configurations.FundamentalAgentConfiguration;
+import Configurations.TraderConfiguration;
+import Enums.Decision;
 
-public class Fundamentalist{
+import java.util.Random;
+import static java.lang.Math.*;
+
+public class Fundamentalist extends Trader {
+    static FundamentalAgentConfiguration configuration;
+    // configuration is static to be used in method updateFundamentalValue()
+    private static Double fundamentalValue = 990.0;
+    // fundamental value is related to assets not to the fundamental trader itself.
+
+
+    public Fundamentalist(TraderConfiguration config) {
+        super(config);
+       // fundamentalValue = configuration.InitialFundamentalValue;
+        // the line above should be removed if FundamentalValue is static
+    }
+
+    public static void updateFundamentalValue(){
+        Random r = new Random();
+        fundamentalValue = fundamentalValue * exp(configuration.ReactionCoefficient* r.nextGaussian());
+    }
+
+    @Override
+    public Decision decideBuyOrSell() {
+        Double value = configuration.ReactionCoefficient * (fundamentalValue - configuration.market.getCurrentPrice());
+        // apply sign (sgn) function to determine the direction of order
+        if(value > 0) // direction = 1;
+        {return Decision.Buy;}
+        else if(value < 0) //direction = -1
+        { return Decision.Sell;}
+        else {return null;} //
+
+
+    }
+
+    @Override
+    public Integer getDesiredOrderVolume() {
+        double orderVolume =abs(fundamentalValue - configuration.market.getCurrentPrice());
+        return (int) orderVolume;
+    }
 }
