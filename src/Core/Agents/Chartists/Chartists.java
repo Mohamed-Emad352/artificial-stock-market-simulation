1964c7261de7adbc9d80dd41c181560c7ed8b787
Mohamed Emad <mem@luftborn.com>
2023-05-25 17:59:59 +0300

    Add timeframes to SimulationParameters.java

--- src/Core/Agents/Chartists/Chartists.java
+++ src/Core/Agents/Chartists/Chartists.java
@@ -1,6 +1,6 @@
 package Core.Agents.Chartists;
 
-import Core.Main;
+import Core.Configurations.SimulationParameters;
 import Core.Agents.Trader;
 import Core.Enums.ChartistType;
 import Core.Enums.Decision;
@@ -9,7 +9,6 @@ import Core.Market.Market;
 
 public class Chartists extends Trader {
     public ChartistType type;
-    int movingAverageWindowSize = Main.randGenr.nextInt(240) + 1;
     TechnicalIndicator TI;
     float forecastValue;
 
@@ -21,38 +20,34 @@ public class Chartists extends Trader {
 
     public void getValueNew() {
         switch (type) {
-            case SimpleMovingAverage -> forecastValue = TI.calculateSMAIndicator(150, Market.closePrices);
-            case ExpMovingAverage -> forecastValue = TI.calculateEMAIndicator(150, Market.closePrices);
-            case DoubleExpMovingAverage -> forecastValue = TI.calculateDoubleEMAIndicator(150, Market.closePrices);
-            case TripleExpMovingAverage -> forecastValue = TI.calculateTripleEMAIndicator(150, Market.closePrices);
-            case KAMA -> forecastValue = TI.calculateKAMAIndicator(70, 70, 100, Market.closePrices);
-            case MACD -> forecastValue = TI.calculateMACDIndicator(100, 150, Market.closePrices);
-            case RAVI -> forecastValue = TI.calculateRAVIIndicator(100, 150, Market.closePrices);
-            case ROC -> forecastValue = TI.calculateROCIndicator(150, Market.closePrices);
-            case WMA -> forecastValue = TI.calculateWMAIndicator(150, Market.closePrices);
-            case ZLEMA -> forecastValue = TI.calculateZLEMAIndicator(150, Market.closePrices);
-//            case DMI ->
-//                    forecastValue = TI.calculateDMIndicator(10, Market.highPrices, Market.lowPrices, Market.closePrices);
-//            case ADMI ->
-//                    forecastValue = TI.calculateADMIndicator(10, Market.highPrices, Market.lowPrices, Market.closePrices);
-            case HMA -> forecastValue = TI.calculateHMAIndicator(150, Market.closePrices);
-            case CoppCurve -> forecastValue = TI.calculateCoppockCurveIndicator(150, 150, 120, Market.closePrices);
-            case RSI -> forecastValue = TI.calculateRSIIndicator(150, Market.closePrices);
+            case SimpleMovingAverage -> forecastValue = TI.calculateSMAIndicator(SimulationParameters.timeFrame, Market.closePrices);
+            case ExpMovingAverage -> forecastValue = TI.calculateEMAIndicator(SimulationParameters.timeFrame, Market.closePrices);
+            case DoubleExpMovingAverage -> forecastValue = TI.calculateDoubleEMAIndicator(SimulationParameters.timeFrame, Market.closePrices);
+            case TripleExpMovingAverage -> forecastValue = TI.calculateTripleEMAIndicator(SimulationParameters.timeFrame, Market.closePrices);
+            case KAMA -> forecastValue = TI.calculateKAMAIndicator(SimulationParameters.timeFrameEffectivenessRatio, SimulationParameters.fastTimeFrame, SimulationParameters.slowTimeFrame, Market.closePrices);
+            case MACD -> forecastValue = TI.calculateMACDIndicator(SimulationParameters.shortTimeFrame, SimulationParameters.longTimeFrame, Market.closePrices);
+            case RAVI -> forecastValue = TI.calculateRAVIIndicator(SimulationParameters.shortTimeFrame, SimulationParameters.longTimeFrame, Market.closePrices);
+            case ROC -> forecastValue = TI.calculateROCIndicator(SimulationParameters.timeFrame, Market.closePrices);
+            case WMA -> forecastValue = TI.calculateWMAIndicator(SimulationParameters.timeFrame, Market.closePrices);
+            case ZLEMA -> forecastValue = TI.calculateZLEMAIndicator(SimulationParameters.timeFrame, Market.closePrices);
+            case HMA -> forecastValue = TI.calculateHMAIndicator(SimulationParameters.timeFrame, Market.closePrices);
+            case CoppCurve -> forecastValue = TI.calculateCoppockCurveIndicator(SimulationParameters.shortRocTimeFrame, SimulationParameters.longRocTimeFrame, SimulationParameters.wmaTimeFrame, Market.closePrices);
+            case RSI -> forecastValue = TI.calculateRSIIndicator(SimulationParameters.timeFrame, Market.closePrices);
             case WilliamR ->
-                    forecastValue = TI.calculateWilliamsRIndicator(150, Market.closePrices, Market.highPrices, Market.lowPrices);
+                    forecastValue = TI.calculateWilliamsRIndicator(SimulationParameters.timeFrame, Market.closePrices, Market.highPrices, Market.lowPrices);
             case AD ->
                     forecastValue = TI.calculateAccumulationDistributionIndicator(Market.highPrices, Market.lowPrices, Market.closePrices,Market.tradeVolumes);
             case Chaikin ->
-                    forecastValue = TI.calculateChaikinMoneyFlowIndicator(100, Market.highPrices, Market.lowPrices, Market.closePrices, Market.tradeVolumes);
+                    forecastValue = TI.calculateChaikinMoneyFlowIndicator(SimulationParameters.timeFrame, Market.highPrices, Market.lowPrices, Market.closePrices, Market.tradeVolumes);
             case VWAP ->
-                    forecastValue = TI.calculateVWAPIndicator(100, Market.highPrices, Market.lowPrices, Market.closePrices, Market.tradeVolumes);
+                    forecastValue = TI.calculateVWAPIndicator(SimulationParameters.timeFrame, Market.highPrices, Market.lowPrices, Market.closePrices, Market.tradeVolumes);
             case MVWAP ->
-                    forecastValue = TI.calculateMVWAPIndicator(100, Market.highPrices, Market.lowPrices, Market.closePrices, Market.tradeVolumes);
+                    forecastValue = TI.calculateMVWAPIndicator(SimulationParameters.timeFrame, Market.highPrices, Market.lowPrices, Market.closePrices, Market.tradeVolumes);
             case NVI -> forecastValue = TI.calculateNVIIndicator(Market.closePrices, Market.tradeVolumes);
             case PVI -> forecastValue = TI.calculatePVIIndicator(Market.closePrices, Market.tradeVolumes);
-            case PPO -> forecastValue = TI.calculatePPOIndicator(100, 150, Market.closePrices);
+            case PPO -> forecastValue = TI.calculatePPOIndicator(SimulationParameters.shortTimeFrame, SimulationParameters.longTimeFrame, Market.closePrices);
             case StochasticOscillatorK ->
-                    forecastValue = TI.calculateStochasticOscillatorKIndicator(100, Market.closePrices, Market.highPrices, Market.lowPrices);
+                    forecastValue = TI.calculateStochasticOscillatorKIndicator(SimulationParameters.timeFrame, Market.closePrices, Market.highPrices, Market.lowPrices);
         }
     }
 
@@ -68,7 +63,6 @@ public class Chartists extends Trader {
             case MACD, PPO -> d = TI.calculateMACDSignal(forecastValue, Market.closePrices);
             case RAVI -> d = TI.calculateRAVISignal(forecastValue, (float) 0.3);
             case ROC, CoppCurve -> d = TI.calculateROCSignal(forecastValue);
-//            case DMI, ADMI -> d = TI.calculateDMISignal(forecastValue, 25);
             case RSI, StochasticOscillatorK -> d = TI.calculateRSISignal(forecastValue, 70, 30);
             case WilliamR -> d = TI.calculateRSISignal(forecastValue, -20, -80);
             case AD, Chaikin -> d = TI.calculateDMISignal(forecastValue, 0);
--- src/Core/Configurations/SimulationParameters.java
+++ src/Core/Configurations/SimulationParameters.java
@@ -6,6 +6,15 @@ public abstract class SimulationParameters {
     public static final double traderQuantityFactor = 0.1;
     public static final int numberOfFundamentalists = 10000;
     public static final int simulationRuns = 30;
+    public static final int timeFrame = 150;
+    public static final int shortTimeFrame = 100;
+    public static final int shortRocTimeFrame = 100;
+    public static final int longTimeFrame = 150;
+    public static final int longRocTimeFrame = 150;
+    public static final int slowTimeFrame = 100;
+    public static final int fastTimeFrame = 70;
+    public static final int timeFrameEffectivenessRatio = 70;
+    public static final int wmaTimeFrame = 120;
     /*
         Use paths relative to the project directory
      */
