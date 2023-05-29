429b74dda4af8ceeb6697e5b4b4e5bc05d3f9b58
Mohamed Emad <mem@luftborn.com>
2023-05-29 17:40:24 +0300

    revert runs to 30

--- src/Core/Configurations/SimulationParameters.java
+++ src/Core/Configurations/SimulationParameters.java
@@ -7,7 +7,7 @@ public abstract class SimulationParameters {
     public static final int tradingDays = 240;
     public static final float traderAggressiveness = 0.001f;
     public static final int numberOfFundamentalists = 10000;
-    public static final int simulationRuns = 1;
+    public static final int simulationRuns = 30;
     public static final int timeFrame = TimeFrame.small;
     public static final float traderConfidenceLevel = ConfidenceLevel.veryLow;
     public static final String relativeStockDataPath = "/src/Core/Data/AMIA stock prices.csv"; // Use paths relative to the project directory
